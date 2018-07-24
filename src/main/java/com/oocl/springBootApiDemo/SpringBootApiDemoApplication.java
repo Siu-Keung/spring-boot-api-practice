140f7e9d0a4ee9a668f9758078d74e04c296e250
SiuKeung <siu_keung@outlook.com>
2018-07-24 12:34:14 +0800

    构建了项目

---
+++ src/main/java/com/oocl/springBootApiDemo/SpringBootApiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.springBootApiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiDemoApplication.class, args);
+	}
+}
